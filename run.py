215309c584a1ba2ad701bcfbf502f28966bbc5df
GTOMMYN <98887801+GTOMMYN@users.noreply.github.com>
2022-04-26 09:22:43 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 from waitress import serve
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080, url_scheme='https')
+    serve(app, host='0.0.0.0', port=80, url_scheme='https')
