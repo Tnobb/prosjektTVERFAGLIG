aecf89347369310896d77dde507fdae3a3819d9b
GTOMMYN <98887801+GTOMMYN@users.noreply.github.com>
2022-05-02 13:05:51 +0200

    alle sider lag

--- app/views.py
+++ app/views.py
@@ -7,6 +7,22 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-@app.route('/dokumentasjon')
-def dokumentasjon():
-    return render_template('dokumentasjon.html')
+@app.route('/chromebook')
+def chromebook():
+    return render_template('chromebook.html')
+
+@app.route('/maskinvare')
+def maskinvare():
+    return render_template('maskinvare.html')
+
+@app.route('/ansatte')
+def ansatte():
+    return render_template('ansatte.html')
+
+@app.route('/programvare')
+def programvare():
+    return render_template('programvare.html')
+
+@app.route('/hurtigtaster')
+def hurtigtaster():
+    return render_template('hurtigtaster.html')
